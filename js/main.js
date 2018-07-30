3e88e5df4b8bd303b20fbb61a21d6af5238d2443
Felipe Gomes <felipe.pbgomes@gmail.com>
2018-07-30 00:01:02 -0300

    now added the function for all patients and validation of Height and Weight

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
-var title = document.querySelector(".title");
+let title = document.querySelector(".title");
 
 title.textContent = "Aparecida Nutricionista";
 
-var patient = document.querySelector("#first-patient");
+let patients = document.querySelectorAll(".paciente");
 
-var weight = patient.querySelector(".info-peso").textContent;
-var height = patient.querySelector(".info-altura").textContent;
+for (let i = 0; i < patients.length; i++) {
+  let patient = patients[i];
 
-var imc = weight / (height * height);
+  let tdWeight = patient.querySelector(".info-peso")
+  let tdHeight = patient.querySelector(".info-altura");
+  let tdImc = patient.querySelector(".info-imc");
 
-patient.querySelector(".info-imc").textContent = imc;
+  let weight = tdWeight.textContent;
+  let height = tdHeight.textContent;
+
+  let weightIsValid = true;
+  let heightIsValid = true;
+
+  if (weight <= 0 || weight >= 1000) {
+    console.log("Invalid Weight");
+    tdWeight.textContent = "Invalid Weight!";
+    tdImc.textContent = "Invalid Weight!";
+    weightIsValid = false;
+    patient.classList.add("paciente-invalido");
+  }
+
+  if (height <= 0 || height >= 3.00) {
+    console.log("Invalid Height");
+    tdHeight.textContent = "Invalid Height!";
+    tdImc.textContent = "Invalid Height!";
+    heightIsValid = false;
+    patient.classList.add("paciente-invalido");
+  }
+
+  if (heightIsValid && weightIsValid) {
+    let imc = weight / (height * height);
+    tdImc.textContent = imc.toFixed(2);
+  }
+}
